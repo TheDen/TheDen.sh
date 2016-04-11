cbef7c5064eb87045682087fb3f92961991884e1
denis <deniskhoshaba@gmail.com>
2016-04-11 10:21:24 +1000

    move scripts out of html

---
+++ scripts/drag.js
@@ -0,0 +1,15 @@
+var elem = document.querySelector('.draggable');
+var draggie = new Draggabilly( elem, {
+    });
+var draggie = new Draggabilly( '.draggable', {
+    });
+var draggableElems = document.querySelectorAll('.draggable');
+        var draggies = []
+
+	    for ( var i=0, len = draggableElems.length; i < len; i++ ) {
+		var draggableElem = draggableElems[i];
+		var draggie = new Draggabilly( draggableElem, {
+
+		    });
+		draggies.push( draggie );
+	    }

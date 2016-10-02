807307f1e4c6a2a13dfb627fd55ce2d382239e6c
denis <deniskhoshaba@gmail.com>
2016-10-02 21:55:25 +1100

    add jitter

---
+++ scripts/jitter.js
@@ -0,0 +1,23 @@
+function jitter() {
+//  console.log('1st');
+  myElement.style.textShadow = "4px 0px 1px";
+}
+
+ function nojitter() {
+//   console.log('2nd')
+  //myElement = document.querySelector("body");
+  myElement.style.textShadow = "0px 0px 1px";
+}
+
+function start() {
+//var myElement = document.querySelector("body");
+max = 12000;
+min = 8000;
+sleep = Math.random() * (max - min) + min;
+setTimeout(jitter, sleep);
+setTimeout(nojitter, sleep+200);
+setTimeout(start, 3000);
+}
+
+var myElement = document.querySelector("body");
+start();

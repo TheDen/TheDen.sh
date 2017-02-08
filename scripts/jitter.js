4cd6edf73de73d83ce97b9130c2a83d92910c165
Patchy <pnx-bot@previousnext.com.au>
2017-02-09 07:57:16 +1100

    disbale jitter for mobiles

--- scripts/jitter.js
+++ scripts/jitter.js
@@ -1,26 +1,33 @@
-function jitter() {
-//  console.log('1st');
-  cardcontent.style.textShadow = "4px 0px 1px";
-  backcolor.style.mixBlendMode = "luminosity";
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
- function nojitter() {
-//   console.log('2nd')
-  //cardcontent = document.querySelector("body");
-  cardcontent.style.textShadow = "0px 0px 1px";
-  backcolor.style.mixBlendMode = "normal";
-}
+if (!isMobile()) {
 
-function start() {
-//var cardcontent = document.querySelector("body");
-max = 12000;
-min = 20000;
-sleep = Math.random() * (max - min) + min;
-setTimeout(jitter, sleep);
-setTimeout(nojitter, sleep+200);
-setTimeout(start, 5000);
-}
+  function jitter() {
+    //  console.log('1st');
+    cardcontent.style.textShadow = "4px 0px 1px";
+    backcolor.style.mixBlendMode = "luminosity";
+  }
 
-var cardcontent = document.querySelector("#cardcontent");
-var backcolor = document.querySelector("html");
-start();
+  function nojitter() {
+    //   console.log('2nd')
+    //cardcontent = document.querySelector("body");
+    cardcontent.style.textShadow = "0px 0px 1px";
+    backcolor.style.mixBlendMode = "normal";
+  }
+
+  function start() {
+    //var cardcontent = document.querySelector("body");
+    max = 12000;
+    min = 20000;
+    sleep = Math.random() * (max - min) + min;
+    setTimeout(jitter, sleep);
+    setTimeout(nojitter, sleep+200);
+    setTimeout(start, 5000);
+  }
+
+  var cardcontent = document.querySelector("#cardcontent");
+  var backcolor = document.querySelector("html");
+  start();
+}

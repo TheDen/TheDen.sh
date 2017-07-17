a6948cad9c9f468126e5ed7a0df18c6ad04ffdcb
TheDen <deniskhoshaba@gmail.com>
2017-07-18 02:29:53 +1000

    cleanup

--- scripts/jitter.js
+++ scripts/jitter.js
@@ -5,20 +5,16 @@ function isMobile() {
 if (!isMobile()) {
 
   function jitter() {
-    //  console.log('1st');
     cardcontent.style.textShadow = "4px 0px 1px";
     backcolor.style.mixBlendMode = "luminosity";
   }
 
   function nojitter() {
-    //   console.log('2nd')
-    //cardcontent = document.querySelector("body");
     cardcontent.style.textShadow = "0px 0px 1px";
     backcolor.style.mixBlendMode = "normal";
   }
 
   function start() {
-    //var cardcontent = document.querySelector("body");
     max = 12000;
     min = 20000;
     sleep = Math.random() * (max - min) + min;

0fd6237e9710d6d2227f422a1074ffca178b6651
TheDen <deniskhoshaba@gmail.com>
2021-01-27 01:12:24 +1100

    minify

--- sync.sh
+++ sync.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 rsync -av . dist/ --exclude dist/ --exclude .git/ --exclude .gitignore
+
+echo "Minifying everything we can"
+find ./dist/ -type f \( \
+  -name "*.html" \
+  -o -name '*.js' \
+  -o -name '*.css' \
+  -o -name '*.svg' \
+  -o -name "*.xml" \
+  -o -name "*.json" \
+  -o -name "*.htm" \
+  \) \
+  -and ! -name "*.min*" -print0 |
+  xargs -0 -n1 -P4 -I '{}' sh -c 'minify -o "{}" "{}"'

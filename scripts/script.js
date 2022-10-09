f4c4524645d670fa581453c1c8bfa0ce0f1feeae
TheDen <deniskhoshaba@gmail.com>
2022-10-09 21:43:33 +1100

    remove console.log

--- scripts/script.js
+++ scripts/script.js
@@ -329,7 +329,6 @@ tabInfo();
 /* Visitor Info */
 $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
     ipinfo = data
-    console.log(ipinfo.ip)
     if (ipinfo.ip != null) {
       $('.ipaddress').html(ipinfo.ip)
       $(".ipinfo").css("display", "revert")

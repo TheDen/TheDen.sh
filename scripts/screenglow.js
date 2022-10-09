39d83b96ef03109981597bc888bca1ebdf7e4165
TheDen <deniskhoshaba@gmail.com>
2022-10-09 12:55:35 +1100

    clean up jitter + glow

--- scripts/screenglow.js
+++ scripts/screenglow.js
@@ -1,16 +1,31 @@
+function screenon()  {
+  document.querySelector("html").setAttribute("style", "-webkit-filter: blur(0px)  saturate(10)");
+}
+function screenoff()  {
+  setTimeout(document.querySelector("html").setAttribute("style", "-webkit-filter: none"), 114200);
+}
+
+function glow() {
+	var ontime = 1500;
+	var flick = 100;
+	setTimeout(screenon, 0);
+	setTimeout(screenoff, flick);
+	setTimeout(screenon, flick*2);
+	setTimeout(screenoff, ontime);
+	setTimeout(screenon, ontime+flick);
+	setTimeout(screenoff, ontime+flick*2);
+}
+
 cheet('r e t r o', function () {
-  function screenon()  {
-    document.querySelector("html").setAttribute("style", "-webkit-filter: blur(1px)  saturate(10)");
-  }
-  function screenoff()  {
-    setTimeout(document.querySelector("html").setAttribute("style", "-webkit-filter: none"), 114200);
-  }
-  var ontime = 5000;
-  var flick = 40;
-  setTimeout(screenon, 0);
-  setTimeout(screenoff, flick);
-  setTimeout(screenon, flick*2);
-  setTimeout(screenoff, ontime);
-  setTimeout(screenon, ontime+flick);
-  setTimeout(screenoff, ontime+flick*2);
+  glow()
 });
+
+function startglow() {
+  min = 10000;
+	max = 20000;
+  sleep = Math.random() * (max - min) + min;
+	setTimeout(glow, sleep);
+	setTimeout(startglow, 35000);
+}
+
+startglow()

207deb4ab0232ba9bb5a52d50c3f642c3ef8a181
TheDen <deniskhoshaba@gmail.com>
2022-10-14 21:48:49 +1100

    update sync script

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@
   cd scripts
   cat jquery.min.js draggabilly.pkgd.min.js moment.min.js typed.js powerglitch.min.js > vendor.js
 )
-rsync -av . dist/ --exclude dist/ --exclude .git/ --delete --exclude .gitignore
+rsync --exclude=dist/ --exclude=.git/ --exclude=.gitignore --delete -av . dist/
 
 echo "Minifying everything we can"
 find ./dist/ -type f \( \
